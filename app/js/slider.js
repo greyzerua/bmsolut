ac05fc57df29809e60e4ce565f10997883f1237e
greyzerua <greyzerua@ukr.net>
2024-10-25 20:29:05 +0300

    new section

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,29 +1,13 @@
 let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-});
-// Функции для отключения и включения pointer-events на iframe
-function disableIframePointerEvents() {
-  document.querySelectorAll(".swiper-slide iframe").forEach((iframe) => {
-    iframe.style.pointerEvents = "none";
-  });
-}
-
-function enableIframePointerEvents() {
-  document.querySelectorAll(".swiper-slide iframe").forEach((iframe) => {
-    iframe.style.pointerEvents = "auto";
-  });
-}
-// Отключаем pointer-events при начале свайпа
-swiper.on("slideChangeTransitionStart", disableIframePointerEvents);
-swiper.on("slideChangeTransitionEnd", enableIframePointerEvents);
-
-// Отключаем pointer-events для мыши
-document.querySelectorAll(".swiper-slide").forEach((slide) => {
-  slide.addEventListener("mousedown", disableIframePointerEvents);
-  slide.addEventListener("mouseup", enableIframePointerEvents);
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+  }
 });
