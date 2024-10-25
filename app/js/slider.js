6eae94d942f75995c73b4a7ef31696c531d8a8d7
greyzerua <greyzerua@ukr.net>
2024-10-25 14:49:02 +0300

    fix url background

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,8 +1,29 @@
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+// Функции для отключения и включения pointer-events на iframe
+function disableIframePointerEvents() {
+  document.querySelectorAll(".swiper-slide iframe").forEach((iframe) => {
+    iframe.style.pointerEvents = "none";
+  });
+}
+
+function enableIframePointerEvents() {
+  document.querySelectorAll(".swiper-slide iframe").forEach((iframe) => {
+    iframe.style.pointerEvents = "auto";
+  });
+}
+// Отключаем pointer-events при начале свайпа
+swiper.on("slideChangeTransitionStart", disableIframePointerEvents);
+swiper.on("slideChangeTransitionEnd", enableIframePointerEvents);
+
+// Отключаем pointer-events для мыши
+document.querySelectorAll(".swiper-slide").forEach((slide) => {
+  slide.addEventListener("mousedown", disableIframePointerEvents);
+  slide.addEventListener("mouseup", enableIframePointerEvents);
 });
