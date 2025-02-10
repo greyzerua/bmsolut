8dfb1bf069cdcd42a073e43e8a2d38315d5245fb
greyzerua <greyzerua@ukr.net>
2025-02-10 15:09:24 +0200

    add mobile logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,80 +1,71 @@
 import "./countdown-timer.js";
-import { onLoad as onGalleryLoad } from "./gallery.js";
+import {onLoad as onGalleryLoad} from "./gallery.js";
 
 let loader = document.getElementById("preloader");
 let body = document.querySelector("body");
-
-  document.addEventListener("DOMContentLoaded", function () {
-  let img = document.querySelector(".main__img.desktop-photo");
-
-  if (img.complete) {
-    hideLoader();
-  } else {
-    img.addEventListener("load", hideLoader);
-    img.addEventListener("error", hideLoader);
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    let img = document.querySelector(".main__img.desktop-photo");
+    if (img.complete) {
+        hideLoader();
+    } else {
+        img.addEventListener("load", hideLoader);
+        img.addEventListener("error", hideLoader);
+    }
 });
 
 function hideLoader() {
-  loader.style.display = "none";
-  body.style.overflow = "";
+    loader.style.display = "none";
+    body.style.overflow = "";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  onGalleryLoad();
+    onGalleryLoad();
 });
-
 const hideDropdown = (dropdown) => {
-  const content = dropdown.querySelector(".dropdown-content");
-  setTimeout(() => {
-    content.style.maxHeight = null;
-  }, 4);
-  dropdown.classList.remove("active");
-}
-
-const els = document.querySelectorAll(".dropdown-header, .dropdown-button");
-
-
-els.forEach((header) => {
-  header.addEventListener("click", function () {
-    const allDropDowns = document.querySelectorAll(".dropdown-outer");
-    
-    const dropdown = this.closest(".dropdown-outer");
     const content = dropdown.querySelector(".dropdown-content");
-
-    if (dropdown.classList.contains("active")) {
-      hideDropdown(dropdown);
-    } else {
-      
-      allDropDowns.forEach(el => {
-        if (el.classList.contains('active')) {
-          hideDropdown(el);
+    setTimeout(() => {
+        content.style.maxHeight = null;
+    }, 4);
+    dropdown.classList.remove("active");
+}
+const onVideoDropdownOpen = (dropdownId) => {
+    document.querySelector('#' + dropdownId)
+        .querySelectorAll(".swiper-slide").forEach((slide) => {
+        if (slide.classList.contains("video-inserted")) {
+            return;
         }
-      });
-      content.style.maxHeight = content.scrollHeight + "px";
-      dropdown.classList.add("active");
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".swiper-slide").forEach((slide) => {
-    slide.addEventListener("click", function () {
-      let videoUrl = this.getAttribute("data-video"); // mute=1 для мобильных устройств
-      let iframe = document.createElement("iframe");
-
-      iframe.setAttribute("src", videoUrl);
-      iframe.setAttribute("frameborder", "0");
-      iframe.setAttribute("allow", "autoplay; encrypted-media");
-      iframe.setAttribute("allowfullscreen", "true");
-
-
-      this.innerHTML = "";
-      this.appendChild(iframe);
-
-      setTimeout(() => {
-        iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
-      }, 500);
+        let videoUrl = slide.getAttribute("data-video");
+        if (videoUrl) {
+            let iframe = document.createElement("iframe");
+            iframe.setAttribute("src", videoUrl);
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allow", "autoplay; encrypted-media");
+            iframe.setAttribute("allowfullscreen", "true");
+            slide.innerHTML = "";
+            slide.appendChild(iframe);
+            slide.classList.add("video-inserted");
+        }
     });
-  });
+}
+const els = document.querySelectorAll(".dropdown-header, .dropdown-button");
+els.forEach((header) => {
+    header.addEventListener("click", function () {
+        const allDropDowns = document.querySelectorAll(".dropdown-outer");
+        const dropdown = this.closest(".dropdown-outer");
+        const content = dropdown.querySelector(".dropdown-content");
+        if (dropdown.classList.contains("active")) {
+            hideDropdown(dropdown);
+        } else {
+            allDropDowns.forEach(el => {
+                if (el.classList.contains('active')) {
+                    hideDropdown(el);
+                }
+            });
+            content.style.maxHeight = content.scrollHeight + "px";
+            dropdown.classList.add("active");
+            if (['video-dropdown', 'video-dropdown-business'].includes(dropdown.id)) {
+                onVideoDropdownOpen(dropdown.id);
+            }
+        }
+    });
 });
